08775cd523cff8101a92c571f6a8f6387b23d526
Saade <saade@outlook.com.br>
2024-03-28 10:27:58 -0300

    fix: event not rendering due to always defining eventContent method

--- src/Widgets/Concerns/InteractsWithRawJS.php
+++ src/Widgets/Concerns/InteractsWithRawJS.php
@@ -9,14 +9,13 @@ trait InteractsWithRawJS
      * If supplied as a callback function, it is called every time the associated event data changes.
      *
      * @see https://fullcalendar.io/docs/event-render-hooks
+     *
      * @return string
      */
     public function eventClassNames(): string
     {
         return <<<JS
-            function ({ event, timeText, isStart, isEnd, isMirror, isPast, isFuture, isToday, el, view }){
-                // Write your own custom implementation here
-            }
+            null
         JS;
     }
 
@@ -25,14 +24,13 @@ trait InteractsWithRawJS
      * If supplied as a callback function, it is called every time the associated event data changes.
      *
      * @see https://fullcalendar.io/docs/event-render-hooks
+     *
      * @return string
      */
     public function eventContent(): string
     {
         return <<<JS
-            function ({ event, timeText, isStart, isEnd, isMirror, isPast, isFuture, isToday, el, view }){
-                // Write your own custom implementation here
-            }
+            null
         JS;
     }
 
@@ -40,14 +38,13 @@ trait InteractsWithRawJS
      * Called right after the element has been added to the DOM. If the event data changes, this is NOT called again.
      *
      * @see https://fullcalendar.io/docs/event-render-hooks
+     *
      * @return string
      */
     public function eventDidMount(): string
     {
         return <<<JS
-            function ({ event, timeText, isStart, isEnd, isMirror, isPast, isFuture, isToday, el, view }){
-                // Write your own custom implementation here
-            }
+            null
         JS;
     }
 
@@ -55,14 +52,13 @@ trait InteractsWithRawJS
      * Called right before the element will be removed from the DOM.
      *
      * @see https://fullcalendar.io/docs/event-render-hooks
+     *
      * @return string
      */
     public function eventWillUnmount(): string
     {
         return <<<JS
-            function ({ event, timeText, isStart, isEnd, isMirror, isPast, isFuture, isToday, el, view }){
-                // Write your own custom implementation here
-            }
+            null
         JS;
     }
 }
