69ef06f3cedd57fb2ee616bd6d4f8a9085bd9eb5
Valentin Fily <valentin@bailfacile.fr>
2024-12-03 19:41:27 +0100

    Add draggable events

---
+++ resources/views/fullcalendar.blade.php
@@ -0,0 +1,41 @@
+@php
+    $plugin = \Saade\FilamentFullCalendar\FilamentFullCalendarPlugin::get();
+@endphp
+
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <div class="flex justify-end flex-1 mb-4">
+            <x-filament-actions::actions :actions="$this->getCachedHeaderActions()" class="shrink-0" />
+        </div>
+
+        @if ($plugin->isEditable() && $plugin->isDroppable())
+            <div id='draggable-events'>
+                @foreach ($this->draggableEvents() as $draggableEvent)
+                    <x-filament::button outlined class='fc-event'
+                        data-event="{{ json_encode($draggableEvent) }}">{{ $draggableEvent['title'] }}</x-filament::button>
+                @endforeach
+            </div>
+        @endif
+
+        <div class="filament-fullcalendar" wire:ignore ax-load
+            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-fullcalendar-alpine', 'saade/filament-fullcalendar') }}"
+            ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-fullcalendar-styles', 'saade/filament-fullcalendar') }}"
+            x-ignore x-data="fullcalendar({
+                locale: @js($plugin->getLocale()),
+                plugins: @js($plugin->getPlugins()),
+                schedulerLicenseKey: @js($plugin->getSchedulerLicenseKey()),
+                timeZone: @js($plugin->getTimezone()),
+                config: @js($this->getConfig()),
+                editable: @json($plugin->isEditable()),
+                droppable: @json($plugin->isDroppable()),
+                selectable: @json($plugin->isSelectable()),
+                eventClassNames: {!! htmlspecialchars($this->eventClassNames(), ENT_COMPAT) !!},
+                eventContent: {!! htmlspecialchars($this->eventContent(), ENT_COMPAT) !!},
+                eventDidMount: {!! htmlspecialchars($this->eventDidMount(), ENT_COMPAT) !!},
+                eventWillUnmount: {!! htmlspecialchars($this->eventWillUnmount(), ENT_COMPAT) !!},
+            })">
+        </div>
+    </x-filament::section>
+
+    <x-filament-actions::modals />
+</x-filament-widgets::widget>
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+const preset = require('./vendor/filament/filament/tailwind.config.preset')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    presets: [preset],
+    content: ['./resources/views/**/*.blade.php', './src/**/*.php'],
+}
